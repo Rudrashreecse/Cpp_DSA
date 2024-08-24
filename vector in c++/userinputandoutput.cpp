760c81bfb65425a5924eed8b2038b84051532bb6
Rudra <rudrashreenayak2005@gmail.com>
2024-08-25 01:19:39 +0530

    25-08-24

--- vector in c++/userinputandoutput.cpp
+++ vector in c++/userinputandoutput.cpp
@@ -7,8 +7,11 @@ cout<<"enter the elemet of vector "<<endl;
 for(int i=0; i<=4;i++){
     cin>>v[i];
 }
+
+v.erase(v.begin()+1,v.begin()+4);
 cout<<"the vector is ";
-for(int i=0; i<=4;i++){
+for(int i=0; i<v.size();i++){
     cout<<v[i]<<" ";
 }
+cout<<endl;
 }
